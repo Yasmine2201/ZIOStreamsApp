c2d42ec6887a0516fa73fb496ff2da631209af56
ThibClnt <thibaut.colnot@efrei.net>
2024-01-19 02:33:10 +0100

    Put back consoleLoop in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,26 +10,7 @@ object Main extends ZIOAppDefault {
   override def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Unit] = {
     for {
       data <- loadData
-      _ <- printLine(
-        GlobalStatisticsAnalysis.formatStatisticsTable(
-          "Environmental impact of electricity production and consumption",
-          GlobalStatisticsAnalysis.getCarbonIntensityStatistics(data, LocalDate.of(2022, 1, 1), LocalDate.of(2022, 12, 31))
-        )
-      )
-      _ <- printLine(
-        "\n\n" +
-          GlobalStatisticsAnalysis.formatStatisticsTable(
-            "Electricty consumption and temperature",
-            GlobalStatisticsAnalysis.getTemperatureAndConsumptionStatistics(data, LocalDate.of(2022, 1, 1), LocalDate.of(2022, 12, 31))
-          )
-      )
-      _ <- printLine(
-        "\n\n" +
-          GlobalStatisticsAnalysis.formatStatisticsTable(
-            "Production (MW) by supply chain",
-            GlobalStatisticsAnalysis.getProductionBySupplyChain(data, LocalDate.of(2022, 1, 1), LocalDate.of(2022, 12, 31))
-          )
-      )
+      _    <- consoleLoop(data)
     } yield ()
   }
 }
